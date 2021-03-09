d571a8dee8fe692065b1a586a4ef46b94e20049b
Pratik Thanki <pratikthanki1@gmail.com>
2021-03-09 23:37:38 +0000

    Pass headers to requests

--- src/Benchy/Configuration/Configuration.cs
+++ src/Benchy/Configuration/Configuration.cs
@@ -41,11 +41,6 @@ namespace Benchy.Configuration
         public List<Stage> Stages { get; set; }
 
         /// <summary>
-        /// Set the User Agent for all requests
-        /// </summary>
-        public string UserAgent { get; set; }
-
-        /// <summary>
         /// Set seed to create reproducible test runs
         /// </summary>
         public int RandomSeed { get; set; }
@@ -54,6 +49,11 @@ namespace Benchy.Configuration
         /// Whether to print to the console
         /// </summary>
         public bool ConsoleLog { get; set; }
+        
+        /// <summary>
+        /// Set of headers to pass with the query
+        /// </summary>
+        public Dictionary<string, string> Headers { get; set; }
     }
 
     public class Stage
@@ -67,5 +67,10 @@ namespace Benchy.Configuration
         /// Concurrent (user) requests to make 
         /// </summary>
         public int VirtualUsers { get; set; }
+
+        public override string ToString()
+        {
+            return $"Requests: {Requests}; VirtualUsers: {VirtualUsers}";
+        }
     }
 }
---
+++ src/Benchy/Helpers/RequestClient.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Benchy.Configuration;
+using Benchy.Models;
+using Microsoft.Extensions.Logging;
+
+namespace Benchy.Helpers
+{
+    public interface IRequestClient
+    {
+        Task<RequestSummary> RecordRequestAsync(
+            string url,
+            Stage stage,
+            Dictionary<string, string> headers,
+            CancellationToken cancellationToken);
+    }
+
+    public class RequestClient : IRequestClient
+    {
+        private readonly ILogger<RequestClient> _logger;
+        private readonly ITimeHandler _timeHandler;
+
+        public RequestClient(ILogger<RequestClient> logger, ITimeHandler timeHandler)
+        {
+            _logger = logger;
+            _timeHandler = timeHandler;
+        }
+
+        public async Task<RequestSummary> RecordRequestAsync(
+            string url,
+            Stage stage,
+            Dictionary<string, string> headers,
+            CancellationToken cancellationToken)
+        {
+            var report = new RequestSummary
+            {
+                Id = Guid.NewGuid(), Stage = stage, Url = url, Start = DateTime.UtcNow
+            };
+
+            var httpRequestMessage = BuildHttpRequestMessage(url, headers);
+
+            var client = new HttpClient();
+
+            _timeHandler.Start();
+
+            var response = await client.SendAsync(httpRequestMessage, cancellationToken);
+
+            _timeHandler.Stop();
+            report.End = DateTime.UtcNow;
+
+            report.DurationMs = _timeHandler.ElapsedMilliseconds();
+            report.StatusCode = response.StatusCode;
+
+            _logger.LogInformation($"Request sent: {report}");
+
+            return report;
+        }
+
+        private static HttpRequestMessage BuildHttpRequestMessage(string url, Dictionary<string, string> headers)
+        {
+            var requestMethod = HttpMethod.Get;
+            var httpRequestMessage = new HttpRequestMessage
+            {
+                Method = requestMethod,
+                RequestUri = new Uri(url)
+            };
+
+            foreach (var (key, value) in headers)
+            {
+                httpRequestMessage.Headers.Add(key, value);
+            }
+
+            return httpRequestMessage;
+        }
+    }
+}
