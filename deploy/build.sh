11ce493fdea0cdb45ca486d6bde18e451c5c4bce
Pratik Thanki <pratikthanki1@gmail.com>
2021-03-21 15:28:20 +0000

    Update dockerfile

--- deploy/build.sh
+++ deploy/build.sh
@@ -3,16 +3,19 @@ set -e
 
 trap cleanup ERR TERM INT
 
-echo "Running redis docker container.."
-docker run --name local-redis -d redis
+echo "Killing existing rabbitmq docker container.."
+docker stop rabbitmq || true && docker rm rabbitmq || true
+
+echo "Running rabbitmq docker container.."
+docker run -d --rm -it --hostname my-rabbit --name rabbitmq -p 15672:15672 -p 5672:5672 rabbitmq:3-management
 
 echo "Building Benchy docker image.."
-docker build . -t benchy
+# docker build . -t benchy
 
-echo "Running benchy in producer mode"
+echo "Running benchy in producer mode.."
 # TODO: docker run here
 
-echo "Running benchy in consumer mode"
+echo "Running benchy in consumer mode.."
 # TODO: docker run here
 
-echo "done!"
+echo "Done!"
